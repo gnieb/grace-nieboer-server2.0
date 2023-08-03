dfc72e3533ca25fed637bf67369bc5cc1ef40624
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-03 12:31:34 -0400

    requirements.txt and now resolving datsbase connection error

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, make_response
 from flask_restful import Resource
-from models import Project, ProjectPhoto
 from config import app, api, db
+from models import Project, ProjectPhoto
+
 from flask_cors import CORS
 from sqlalchemy.sql import text
 
@@ -9,6 +10,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CORS(app, supports_credentials=True) 
+
+
+# @app.route('/')
+# def testdb():
+#     try:
+#         db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+#         return '<h1>It works.</h1>'
+#     except Exception as e:
+#         # e holds description of the error
+#         error_text = "<p>The error:<br>" + str(e) + "</p>"
+#         hed = '<h1>Something is broken.</h1>'
+#         return hed + error_text
 
 class Home(Resource):
     def get(self):
