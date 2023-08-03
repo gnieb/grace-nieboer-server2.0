5981d22656ff1e2b32924e9a03b2f5f5b4e8115f
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-03 11:17:24 -0400

    add project and photo routes to app.py and tested successfully

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
 from flask import Flask, jsonify, make_response
 from flask_restful import Resource
-# from models import Project, ProjectPhoto
+from models import Project, ProjectPhoto
 from config import app, api, db
 from flask_cors import CORS
 from sqlalchemy.sql import text
+
+from dotenv import load_dotenv
+load_dotenv()
+
+CORS(app, supports_credentials=True) 
 
 class Home(Resource):
     def get(self):
         return make_response("AYO")
     
+class Projects(Resource):
+    def get(self):
+
+        if len(Project.query.all()) <1:
+            return make_response("Nothing to see here yet!", 200)
+        projects = [p.to_dict() for p in Project.query.all()]
+        return make_response(projects, 200)
+    
+class ProjectPhotos(Resource):
+    def get(self):
+        projphotos = [p.to_dict() for p in ProjectPhoto.query.all()]
+        return make_response(projphotos, 200)
+    
+    
 
 api.add_resource(Home, '/')
+api.add_resource(Projects, '/projects')
+api.add_resource(ProjectPhotos, '/photos')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
