7dbf610e8bcf588cb26c79c17a0b16d4913a5148
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-03 15:03:13 -0400

    add backpopulates back

--- models.py
+++ models.py
@@ -10,6 +10,8 @@ class Project(db.Model, SerializerMixin):
     demo = db.Column(db.String)
     descript = db.Column(db.String)
    
+    photo = db.relationship('ProjectPhoto', uselist=False, back_populates='project')
+
 class ProjectPhoto(db.Model, SerializerMixin):
     __tablename__ = 'projectphotos'
 
@@ -18,3 +20,4 @@ class ProjectPhoto(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False) # This field will store the image file name or path.
     project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), unique=True)
     
+    project = db.relationship('Project', back_populates='photo')
