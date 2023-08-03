987239851c6098d8c941fa1ab76190db43899ce3
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-03 11:11:07 -0400

    config file configured adn complete

---
+++ config.py
@@ -0,0 +1,19 @@
+from flask_restful import Api
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+api = Api(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
